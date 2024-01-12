768e3afb5ed50daa9b6d05206064a202e9ab3b47
JasonLee4 <84427302+JasonLee4@users.noreply.github.com>
2024-01-12 01:56:07 -0600

    Ranged enemy with health bar

---
+++ scenes/projectiles/cornbullet.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+@export var speed = 70
+var direction = Vector2.DOWN
+
+func start(target_dir):
+	target_dir = target_dir
+
+	
+#func _ready():
+	#$SelfdestructTimer.start()
+	
+	
+#
+func _process(delta):
+		translate(direction*speed*delta)
+		
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
+
+
+func _on_body_entered(body):
+	if body.name != "cornpirate":
+		queue_free()
