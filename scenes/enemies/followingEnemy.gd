8f92010aca7903ba5b5c377f15b35d0b366ae8ec
BuildTools <unconfigured@null.spigotmc.org>
2024-01-12 16:04:54 -0600

    Added an enemy superclass

--- scenes/enemies/followingEnemy.gd
+++ scenes/enemies/followingEnemy.gd
@@ -1,22 +1,25 @@
-extends CharacterBody2D
-class_name enemy
+extends enemy
 
-@export var speed = 100
+#Variables for enemy movement
+var pig_position
+var target_position
+@onready var pig = get_parent().get_node("Pig")
 
-var MAX_HEALTH = 100
-var health = MAX_HEALTH
-var damage = 1
-
-var can_attack = true
-var can_attack_player = true
-var player
-
-var invulnerable = false
+var player_inattack_zone = false
+var damaged = false
+var can_take_damage = true
 
 func _physics_process(delta):
-	move()
-	if player != null:
-		deal_damage()
+	deal_with_damage()
+	
+	if(pig != null):
+		pig_position  = pig.global_position
+		target_position = (pig_position-position).normalized()
+	
+	if (position.distance_to(pig_position) > 3):
+		velocity = Vector2(target_position * speed) 
+		move_and_slide()
+		look_at(pig_position)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -41,37 +44,22 @@ func enemy():
 func _on_enemy_hitbox_body_entered(body):
 	if body.has_method("player"):
 		body.enemies_pig_can_attack.append(self)
-		#player_in_hitbox = true
 		player = body
 
 func _on_enemy_hitbox_body_exited(body):
 	if body.has_method("player"):
 		body.enemies_pig_can_attack.erase(self)
-		#player_in_hitbox = false
 		player = null
  
-func move():
-	pass
-
-func deal_damage():
-	if can_attack_player and can_attack:
-		can_attack = false
-		$attack_cooldown.start()
-		player.recieve_damage(damage)
-	pass
-
-func recieve_damage(damage_value):
-	if not invulnerable:
-		health = health - damage_value
+func deal_with_damage():
+	if player_inattack_zone and player.pig_current_attack == true and can_take_damage == true:
+		health = health - 20
 		set_health()
-		invulnerable = true
+		can_take_damage = false
 		$dmg_iframe_cooldown.start()
 		print("Slime health = ", health)
 		if health <= 0:
 			self.queue_free() 
 
 func _on_pig_damage_cooldown_timeout():
-	invulnerable = false
-
-func _on_attack_cooldown_timeout():
-	can_attack = true
+	can_take_damage = true
