efe9e341e384c6f7d958d097466dace5b9f17467
JasonLee4 <84427302+JasonLee4@users.noreply.github.com>
2024-01-20 18:57:27 -0600

    added state machine to pig with new roll or dash

---
+++ scenes/states/state_moving.gd
@@ -0,0 +1,58 @@
+extends PlayerState
+
+@export var max_spd = 100
+@export var accspd = 1500
+var can_roll = false
+var move = Vector2.ZERO
+
+
+func _game_logic(delta):
+	
+	
+	# get movement inputs
+	move = Vector2(int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left")), int(Input.is_action_pressed("down")) - int(Input.is_action_pressed("up")))
+	
+	# if inputs have been pressed
+	if move.length() > 0:
+		# accelerate
+		move = move.normalized()
+		
+		# get the distance
+		var distance = player.velocity.length()
+		distance += (accspd * delta)
+		
+		if distance > max_spd:
+			distance = max_spd
+		
+		# set the velocity
+		player.velocity = move * distance
+	else:
+		
+		# check if snapping to 0 is neccessary
+		accspd = 1500
+		if player.velocity.length() <= (accspd * delta):
+			player.velocity = Vector2(0, 0)
+		else:
+			# otherwise, just decelerate
+			var angle = player.velocity.angle()
+			var new_length = player.velocity.length() - (accspd * delta)
+			
+			player.velocity = Vector2(new_length, 0).rotated(angle)
+
+
+func _transition_logic(existing_states):
+	
+	if can_roll and Input.is_action_just_pressed("dash") and move.length() > 0:
+		# transition to rolling
+		machine.change_state("state_rolling", [move], [])
+
+
+func _enter(args := []):
+	# start the roll cooldown timer
+	can_roll = false
+	$RollCooldown.start()
+
+
+func _on_roll_cooldown_timeout():
+	print("can roll!!")
+	can_roll = true
---
+++ scenes/states/state_rolling.gd
@@ -0,0 +1,47 @@
+extends PlayerState
+
+var move
+@export var roll_spd = 250
+@export var roll_time = 0.35
+@export var invincble_time = 0.45
+
+
+#@onready var face: AnimatedSprite = player.get_node("Sprite/Face")
+var next_state
+
+#var roll_sfx = preload("res://sounds/sfx/dice_roll.wav")
+
+
+func _enter(args := [Vector2.ZERO]):
+	# play sound
+	#Sound.play_sfx(roll_sfx)
+	
+	# set invincibility
+	player.set_invincible(invincble_time)
+	
+	# get move
+	move = args[0]
+	$RollTime.wait_time = roll_time
+	$RollTime.start()
+	
+	#face.playing = true
+	
+	# reroll player weapon
+	next_state = "state_moving"
+	
+
+func _exit(args := []):
+	#player.shield.visible = false
+	pass
+
+
+func _game_logic(delta):
+	player.velocity = move * roll_spd
+	#player.get_node().rotation_degrees += 1440 * delta
+
+
+	
+
+
+func _on_roll_time_timeout():
+	machine.change_state(next_state)
