1264bb29fe4178b1d165d0135067ea91cb6f7826
BuildTools <unconfigured@null.spigotmc.org>
2024-02-02 15:14:10 -0600

    Added simple main menu

---
+++ scenes/levels/main_menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$MarginContainer/VBoxContainer/StartButton.grab_focus()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/levels/main_level.tscn")
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
