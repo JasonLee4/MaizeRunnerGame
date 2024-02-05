fb0b50100e35e9a8359f1ddc24b0e16bf519b603
BuildTools <unconfigured@null.spigotmc.org>
2024-02-05 16:05:22 -0600

    added menu images

--- scenes/levels/main_menu.gd
+++ scenes/levels/main_menu.gd
@@ -3,7 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$MarginContainer/VBoxContainer/StartButton.grab_focus()
+	$Buttons/VBoxContainer/StartButton.grab_focus()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
