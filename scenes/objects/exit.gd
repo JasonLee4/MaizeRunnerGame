fdc5bf73595ba9c8cc39df58239b388e3b4dd0fa
proxim <davidskim04@gmail.com>
2024-02-19 13:43:50 -0600

    fixed bug where you keep key btn lvls

--- scenes/objects/exit.gd
+++ scenes/objects/exit.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
-@onready var key_scene = preload("res://scenes/items/key.tscn")
-
+@onready var key_item : Inv_Item = preload("res://scenes/items/inventory/inv_items/key.tres")
 
 func _on_area_2d_body_entered(body):
 	if not body.has_method("player"):
 		return
-	if Globals.inv.contains(key_scene.instantiate().item):
+	if Globals.inv.contains(key_item):
+		# consume the key
+		Globals.inv.remove_item(key_item, 1)
+		# go to next level
 		Globals.lvl_end.emit()
 		get_tree().change_scene_to_file("res://scenes/menus/you_won_screen.tscn")
