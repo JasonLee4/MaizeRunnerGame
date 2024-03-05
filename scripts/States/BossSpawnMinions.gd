a411acbb0e30fcd95130df47d3dbb66bdeed6a5d
JasonLee4 <84427302+JasonLee4@users.noreply.github.com>
2024-03-05 01:43:52 -0600

    omit reference to missing file

--- scripts/States/BossSpawnMinions.gd
+++ scripts/States/BossSpawnMinions.gd
@@ -10,7 +10,7 @@ var minionsSpawned : int
 @export var boss : CharacterBody2D
 
 const minionSpawnDistance = 50
-@onready var minion_scene = preload("res://scenes/enemies/followingEnemy.tscn")
+#@onready var minion_scene = preload("res://scenes/enemies/followingEnemy.tscn")
 
 #TODO delete after deubgging
 var exited = false
@@ -22,11 +22,12 @@ func enter():
 	spawn_minion()
 	
 func spawn_minion():
-	var minion = minion_scene.instantiate()
-	boss.get_parent().add_child(minion)
-	minion.global_position = boss.global_position+Vector2(randf()*minionSpawnDistance, randf()*minionSpawnDistance)
-	#print(get_parent().get_parent().get_parent())
-	print("Spawning minion at ", minion.global_position)
+	#var minion = minion_scene.instantiate()
+	#boss.get_parent().add_child(minion)
+	#minion.global_position = boss.global_position+Vector2(randf()*minionSpawnDistance, randf()*minionSpawnDistance)
+	##print(get_parent().get_parent().get_parent())
+	#print("Spawning minion at ", minion.global_position)
+	pass
 	
 func update(delta: float):
 	if spawnCooldown > 0:
