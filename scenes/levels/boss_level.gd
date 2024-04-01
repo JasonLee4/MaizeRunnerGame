dfdda1a610090b95c42a5a842592b601849c3544
BuildTools <unconfigured@null.spigotmc.org>
2024-04-01 14:05:06 -0500

    Updated boss level

--- scenes/levels/boss_level.gd
+++ scenes/levels/boss_level.gd
@@ -5,6 +5,8 @@ var player_scene = preload("res://scenes/characters/pig.tscn")
 var boss_enemy = preload("res://scenes/bosses/boss.tscn")
 var laser_res = preload("res://scenes/items/laser_resource.tscn")
 
+var boss_spawned : bool
+
 var ui
 
 @onready var loading_screen = $LoadingScreen
@@ -35,31 +37,35 @@ func _ready():
 	Globals.pig = player
 	player.position = $Player/Marker2D.global_position
 	
-	var boss = boss_enemy.instantiate()
-	$Boss.add_child(boss)
-	boss.position = $Boss/Marker2D.global_position
-	boss.scale = Vector2(2,2)
-	
-	ui.get_node("BossHealthbar").init_boss(boss)
-	
 	Globals.lvl_start.emit()
 	
 	await(get_tree().create_timer(2).timeout)
 	var mono = "Something feels a bit off here..."
 	Globals.monologue.emit(mono)
 	
+	boss_spawned = false
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if(Globals.pig and !boss_spawned):
+		#spawns boss once pig enters the room
+		if (Globals.pig.global_position.x >= $CoalSpawn/Marker2D2.global_position.x+80):
+				var boss = boss_enemy.instantiate()
+				$Boss.add_child(boss)
+				boss.position = $Boss/Marker2D.global_position
+				boss.scale = Vector2(2,2)
+				ui.get_node("BossHealthbar").init_boss(boss)
+				boss_spawned = true
 
 
 func _on_timer_timeout():
-	var lr = laser_res.instantiate()
+	if(boss_spawned):
+		var lr = laser_res.instantiate()
 
-	lr.global_position.x = randf_range($CoalSpawn/Marker2D.global_position.x, $CoalSpawn/Marker2D2.global_position.x)
-	lr.global_position.y = randf_range($CoalSpawn/Marker2D.global_position.y, $CoalSpawn/Marker2D2.global_position.y)
-	
-	add_child(lr)
-	print($Boss.get_child(1).health)
+		lr.global_position.x = randf_range($CoalSpawn/Marker2D.global_position.x, $CoalSpawn/Marker2D2.global_position.x)
+		lr.global_position.y = randf_range($CoalSpawn/Marker2D.global_position.y, $CoalSpawn/Marker2D2.global_position.y)
+		
+		add_child(lr)
+		#print($Boss.get_child(1).health)
--- scripts/States/BossRunAway.gd
+++ scripts/States/BossRunAway.gd
@@ -6,33 +6,43 @@ class_name BossRunAway
 @export var boss : boss
 @export var speed : float
 @export var cam_follow_time : float
+@export var windup_time : float
+@onready var running = false
 var time : float
 
 @export var drop_scene : Resource
 
 func enter():
+	#freeze everything but the boss
+	boss.get_node("boss_music").stop()
+	boss.process_mode = Node.PROCESS_MODE_ALWAYS
+	get_tree().paused = true
+	boss.get_node("CollisionShape2D").disabled = true
 	#fix camera to the boss instead of pig
-	boss.get_node("RunAwayCam").make_current()
+	boss.get_node("BossCam").make_current()
 	#drop key
 	var drop = drop_scene.instantiate()
 	boss.get_parent().add_child(drop)
 	drop.global_position = boss.global_position
-	#play run-away animation
-	run_away()
-	
-func run_away():
-	boss.get_node("AnimationPlayer").play("windup")
-	await get_tree().create_timer(5.0).timeout
-	boss.get_node("AnimationPlayer").stop()
-	time = 0	
+	time = 0
 	
 func update(delta):
-	if time < cam_follow_time:
-		time += delta
-		boss.velocity = Vector2(Vector2(1,0) * speed)
-		boss.move_and_slide()
-	elif time >= cam_follow_time:
-		pig.get_node("Camera2D").make_current()
-		boss.queue_free()	
+	if !running:
+		if time < windup_time:
+			time += delta
+			boss.get_node("AnimationPlayer").play("windup")
+		elif time >= windup_time:
+			boss.get_node("AnimationPlayer").stop()
+			time = 0
+			running = true
+	else:
+		if time < cam_follow_time:
+			time += delta
+			boss.velocity = Vector2(Vector2(1,0) * speed)
+			boss.move_and_slide()
+		elif time >= cam_follow_time:
+			pig.get_node("Camera2D").make_current()
+			boss.queue_free()	
+			get_tree().paused = false
 	
 	
