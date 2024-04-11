abe2c9c9602dac2f2284a5c7b9a0959811fcda7c
JasonLee4 <84427302+JasonLee4@users.noreply.github.com>
2024-04-11 15:41:31 -0500

    added increased flashlight beam range powerup

--- globals/globals.gd
+++ globals/globals.gd
@@ -45,6 +45,8 @@ var laser_energy = 100:
 var apple_damage = 0
 var apple_poison = false
 var wood_craft = 2
+var flash_range = 4
+var flash_area = PackedVector2Array([Vector2(196, -38), Vector2(113, 3), Vector2(196, 41)])
 var onetime_power = []
 
 ### Dungeon ###
@@ -124,12 +126,15 @@ func restart_game():
 			inv.insert(torch_item)
 		for _i in range(5):
 			inv.insert(apple_item)
-	
+		
 func reset_power_ups():
 	
 	apple_damage = 0
 	apple_poison = false
 	wood_craft = 2
+	flash_range = 4
+	flash_area = PackedVector2Array([Vector2(196, -38), Vector2(113, 3), Vector2(196, 41)])
+	
 	onetime_power = []
 
 ### Helper functions ###
---
+++ scenes/menus/power_ups/scripts/increase_flash_range.gd
@@ -0,0 +1,5 @@
+extends Node
+
+func power_up():
+	Globals.flash_range = 6
+	Globals.flash_area = PackedVector2Array([Vector2(226, -54), Vector2(113, 3), Vector2(226, 55)])
