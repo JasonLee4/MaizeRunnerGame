d0c41e984e0dda85276ab9231578fb915904b9a6
proxim <61657192+proxim@users.noreply.github.com>
2024-04-12 14:57:25 -0500

    add particle fx to boss

--- scenes/bosses/boss.gd
+++ scenes/bosses/boss.gd
@@ -4,10 +4,18 @@ class_name boss
 func _ready():
 	MAX_HEALTH = 1000
 	health = 1000
-	damage = 0
+	damage = 1
 	
 	$Healthbar.max_value = MAX_HEALTH
 	set_health()
+
+func _process(_delta):
+	if (health < MAX_HEALTH * .75):
+		$FireSpawn/Fire1.visible = true
+	if (health < MAX_HEALTH * .50):
+		$FireSpawn/Fire2.visible = true
+	if (health < MAX_HEALTH * .25):
+		$FireSpawn/Fire3.visible = true
 
 func boss():
 	pass
