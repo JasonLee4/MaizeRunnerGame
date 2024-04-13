b68b97a50f1034af30b343d435e3d356eb7fab39
JasonLee4 <84427302+JasonLee4@users.noreply.github.com>
2024-04-13 03:03:28 -0500

    added vertical camera shake to boss stomp

--- scenes/levels/Camera.gd
+++ scenes/levels/Camera.gd
@@ -8,12 +8,22 @@ var rng = RandomNumberGenerator.new()
 
 var shake_strength : float = 0.0
 
+var ground_shake = false
+
 func _ready():
 	get_parent().connect("camera_shake", apply_shake)
+	get_parent().connect("ground_shake", apply_shake_boss)
+	
 
 func apply_shake():
+	ground_shake = false
 	shake_strength = randomStrength
 	$AnimationPlayer.play("damage")
+
+func apply_shake_boss():
+	ground_shake = true
+	shake_strength = randomStrength
+	
 
 func _process(delta):
 	if offset.x < 0.1 and offset.y < 0.1:
@@ -25,4 +35,7 @@ func _process(delta):
 		offset = randomOffset()
 
 func randomOffset():
+	if ground_shake:
+		return Vector2(0, rng.randf_range(-shake_strength, shake_strength))
+		
 	return Vector2(rng.randf_range(-shake_strength, shake_strength), rng.randf_range(-shake_strength, shake_strength))
